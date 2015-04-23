6677e1d10497b327dc5a45c3eb0d6ccccba7f072
Robb Shecter <robb@weblaws.org>
2015-04-23 12:10:07 -0700

    Spec passes

--- lib/urbanairship/push/payload.rb
+++ lib/urbanairship/push/payload.rb
@@ -2,10 +2,26 @@ module Urbanairship
   module Push
     module Payload
 
-      def notification(alert: nil)
-        { alert: alert }
+      def notification(alert: nil, ios: nil)
+        if alert
+          { alert: alert }
+        elsif ios
+          { ios: ios }
+        end
       end
 
+      def ios(alert: nil, badge: nil, sound: nil, extra: nil, expiry: nil, category: nil, interactive: nil)
+        {
+          alert: alert,
+          badge: badge,
+          sound: sound,
+          extra: extra,
+          expiry: expiry,
+          category: category,
+          interactive: interactive
+        }
+          .keep_if { |_, value| !value.nil? }
+      end
     end
   end
 end
