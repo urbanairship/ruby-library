c32c61c0d94a53aedf37c06ca8320bc29b45a597
Robb Shecter <robb@weblaws.org>
2015-04-23 22:07:49 -0700

    Failing Push spec

---
+++ spec/lib/urbanairship/push/core_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+require 'urbanairship/push/core'
+include Urbanairship::Push::Core
+
+describe Push do
+  it 'builds a payload structure' do
+    p = Push.new(nil)
+    p.audience = all_
+    p.notification = notification(alert: 'Hello')
+    p.options = options(expiry: 10080)
+    p.device_types = all_
+    p.message = message(
+      title: 'Title',
+      body: 'Body',
+      content_type: 'text/html',
+      content_encoding: 'utf8',
+      extra: { more: 'stuff' },
+      expiry: 10080,
+      icons: { list_icon: 'http://cdn.example.com/message.png' },
+      options: { some_delivery_option: true }
+    )
+
+    expect(p.payload).to eq({
+      'audience': 'all',
+      'notification': {'alert': 'Hello'},
+      'device_types': 'all',
+      'options': { 'expiry': 10080 },
+      'message': {
+        'title': 'Title',
+        'body': 'Body',
+        'content_type': 'text/html',
+        'content_encoding': 'utf8',
+        'extra': {'more': 'stuff'},
+        'expiry': 10080,
+        'icons': { 'list_icon': 'http://cdn.example.com/message.png' },
+        'options': {'some_delivery_option': 'true'},
+      }}
+    )
+  end
+end
