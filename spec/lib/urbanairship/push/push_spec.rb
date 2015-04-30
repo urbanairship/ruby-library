7f27def916c0d745d38755232ac2b57f2b160ed0
Robb Shecter <robb@weblaws.org>
2015-04-30 14:21:40 -0700

    Failing spec for interactive and refactoring

--- spec/lib/urbanairship/push/push_spec.rb
+++ spec/lib/urbanairship/push/push_spec.rb
@@ -27,25 +27,28 @@ describe Push do
       p
     }
 
+    let(:expected_message) {
+      {
+        title: 'Title',
+        body: 'Body',
+        content_type: 'text/html',
+        content_encoding: 'utf8',
+        extra: {more: 'stuff'},
+        expiry: 10080,
+        icons: { list_icon: 'http://cdn.example.com/message.png' },
+        options: { some_delivery_option: true },
+      }
+    }
+
     it 'can build a full payload structure' do
       a_push.notification = notification(alert: 'Hello')
-
       expect(a_push.payload).to eq({
         audience: 'all',
         notification: {alert: 'Hello'},
         device_types: 'all',
         options: { expiry: 10080 },
-        message: {
-          title: 'Title',
-          body: 'Body',
-          content_type: 'text/html',
-          content_encoding: 'utf8',
-          extra: {more: 'stuff'},
-          expiry: 10080,
-          icons: { list_icon: 'http://cdn.example.com/message.png' },
-          options: { some_delivery_option: true },
-        }}
-      )
+        message: expected_message
+      })
     end
 
     it 'can build a payload with actions' do
@@ -62,7 +65,6 @@ describe Push do
           app_defined: {some_app_defined_action: 'some_values'}
         )
       )
-
       expect(a_push.payload).to eq({
         audience: 'all',
         notification: {
@@ -79,17 +81,56 @@ describe Push do
           }},
         device_types: 'all',
         options: { expiry: 10080 },
-        message: {
-          title: 'Title',
-          body: 'Body',
-          content_type: 'text/html',
-          content_encoding: 'utf8',
-          extra: {more: 'stuff'},
-          expiry: 10080,
-          icons: { list_icon: 'http://cdn.example.com/message.png' },
-          options: { some_delivery_option: true },
-        }}
-      )
+        message: expected_message
+      })
     end
+
+    it 'can build a payload with an interactive notification' do
+      a_push.notification = interactive(
+        alert: 'Hello',
+        interactive: interactive(
+          type: 'some_type',
+          button_actions: {
+            yes: {
+              add_tag: 'clicked_yes',
+              remove_tag: 'never_clicked_yes',
+              open: {
+                type: 'url',
+                content: 'http://www.urbanairship.com'
+              }
+            },
+            no: {
+              add_tag: 'hater'
+            }
+          }
+        )
+      )
+      expect(a_push.payload).to eq({
+        audience: 'all',
+        notification: {
+          alert: 'Hello',
+          interactive: {
+            type: 'some_type',
+            button_actions: {
+              yes: {
+                add_tag: 'clicked_yes',
+                remove_tag: 'never_clicked_yes',
+                open: {
+                  type: 'url',
+                  content: 'http://www.urbanairship.com'
+                }
+              },
+              no: {
+                add_tag: 'hater'
+              }
+            }
+          }
+        },
+        device_types: 'all',
+        options: { expiry: 10080 },
+        message: expected_message
+      })
+    end
+
   end
 end
