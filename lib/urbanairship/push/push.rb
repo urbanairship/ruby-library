d582a11e5d9e9c333abb9f4445112b39046860a2
Robb Shecter <robb@weblaws.org>
2015-04-30 12:39:03 -0700

    Docs

--- lib/urbanairship/push/push.rb
+++ lib/urbanairship/push/push.rb
@@ -1,11 +1,15 @@
+# In the Python library, this file is named `core.py`. Here it's
+# `push.rb` in keeping with the Ruby convention of naming the
+# file based on the class it contains.
 module Urbanairship
   module Push
 
+    # A push notification.
     class Push
       def self.initialize(airship)
         @airship = airship
       end
     end
-    
+
   end
 end
