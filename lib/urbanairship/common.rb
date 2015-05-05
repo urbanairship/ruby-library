9d7e12c10429fbfb8e63ba9ffc1f3750be1f7dde
Robb Shecter <robb@weblaws.org>
2015-05-04 19:34:42 -0700

    Urbanairship::Common

---
+++ lib/urbanairship/common.rb
@@ -0,0 +1,16 @@
+module Urbanairship
+  module Common
+    SERVER = 'go.urbanairship.com'
+    BASE_URL = "https://go.urbanairship.com/api"
+    CHANNEL_URL = BASE_URL + '/channels/'
+    DEVICE_TOKEN_URL = BASE_URL + '/device_tokens/'
+    APID_URL = BASE_URL + '/apids/'
+    DEVICE_PIN_URL = BASE_URL + '/device_pins/'
+    PUSH_URL = BASE_URL + '/push/'
+    DT_FEEDBACK_URL = BASE_URL + '/device_tokens/feedback/'
+    APID_FEEDBACK_URL = BASE_URL + '/apids/feedback/'
+    SCHEDULES_URL = BASE_URL + '/schedules/'
+    TAGS_URL = BASE_URL + '/tags/'
+    SEGMENTS_URL = BASE_URL + '/segments/'
+  end
+end
