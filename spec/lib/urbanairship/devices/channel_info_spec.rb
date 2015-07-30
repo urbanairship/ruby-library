d99be03be8f61ded2be5f68aed301c472509ac50
kfrye <kris.frye@gmail.com>
2015-07-30 09:28:31 -0700

    Finished test for ChannelInfo. Added test for ChannelList

--- spec/lib/urbanairship/devices/channel_info_spec.rb
+++ spec/lib/urbanairship/devices/channel_info_spec.rb
@@ -5,44 +5,169 @@ require 'urbanairship/client'
 require 'urbanairship/devices/devicelist'
 
 describe Urbanairship::Devices do
+  UA = Urbanairship
+  airship = UA::Client.new(key: '123', secret: 'abc')
+
   describe Urbanairship::Devices::ChannelInfo do
     lookup_hash = {
-        :channel_id => '123',
-        :device_type => 'ios',
-        :installed => true,
-        :opt_in => false,
-        :push_address => 'FE66489F304DC75B8D6E8200DFF8A456E8DAEACEC428B427E9518741C92C6660',
-        :created => '2013-08-08T20:41:06',
-        :last_registration => '2014-05-01T18:00:27',
-        :alias => 'your_user_id',
-        :tags => %w(tag1 tag2),
-        :tag_groups => {
-            :tag_group_1 => %w(tag1 tag2),
-            :tag_group_2 => %w(tag1 tag2)
-        },
-        :ios => {
-            :badge => 0,
-            :quiettime => {
-                :start => null,
-                :end => null
-            },
-            :tz => 'America/Los_Angeles'
+        'body' => {
+            'channel' => {
+                :channel_id => '123',
+                :device_type => 'ios',
+                :installed => true,
+                :opt_in => false,
+                :push_address => 'FE66489F304DC75B8D6E8200DFF8A456E8DAEACEC428B427E9518741C92C6660',
+                :created => '2013-08-08T20:41:06',
+                :last_registration => '2014-05-01T18:00:27',
+                :alias => 'your_user_id',
+                :tags => %w(tag1 tag2),
+                :tag_groups => {
+                    :tag_group_1 => %w(tag1 tag2),
+                    :tag_group_2 => %w(tag1 tag2)
+                },
+                :ios => {
+                    :badge => 0,
+                    :quiettime => {
+                        :start => nil,
+                        :end => nil
+                    },
+                    :tz => 'America/Los_Angeles'
+                }
+            }
         }
     }
-
-    let(:expected_lookup) { lookup_hash }
+    channel_info = UA::ChannelInfo.new(airship)
 
     describe '#lookup' do
       it 'can get a response' do
-        airship = UA::Client.new(key: '123', secret: 'abc')
-        channel_info = UA::Devices::ChannelInfo.new(airship)
         allow(airship)
             .to receive(:send_request)
-                    .and_return(expected_lookup)
+                    .and_return(lookup_hash)
+
         response = channel_info.lookup('321')
-        expect(response['channel_id']).to eq '123'
+        expect(response[:channel_id]).to eq '123'
       end
     end
+  end
 
+  describe Urbanairship::Devices::ChannelList do
+    channel_list_hash = {
+      'body' => {
+        'channels' => [
+          {
+            :channel_id => '123',
+            :device_type => 'ios',
+            :installed => true,
+            :opt_in => false,
+            :push_address => 'FE66489F304DC75B8D6E8200DFF8A456E8DAEACEC428B427E9518741C92C6660',
+            :created => '2013-08-08T20:41:06',
+            :last_registration => '2014-05-01T18:00:27',
+            :alias => 'your_user_id',
+            :tags => %w(tag1 tag2),
+            :tag_groups => {
+              :tag_group_1 => %w(tag1 tag2),
+              :tag_group_2 => %w(tag1 tag2)
+            },
+            :ios => {
+              :badge => 0,
+              :quiettime => {
+                :start => nil,
+                :end => nil
+              },
+              :tz => 'America/Los_Angeles'
+            }
+          },
+          {
+            :channel_id => '124',
+            :device_type => 'ios',
+            :installed => true,
+            :opt_in => false,
+            :push_address => 'FE66489F304DC75B8D6E8200DFF8A456E8DAEACEC428B427E9518741C92C6660',
+            :created => '2013-08-08T20:41:06',
+            :last_registration => '2014-05-01T18:00:27',
+            :alias => 'your_user_id',
+            :tags => %w(tag1 tag2),
+            :tag_groups => {
+              :tag_group_1 => %w(tag1 tag2),
+              :tag_group_2 => %w(tag1 tag2)
+            },
+            :ios => {
+              :badge => 0,
+              :quiettime => {
+                :start => nil,
+                :end => nil
+              },
+              :tz => 'America/Los_Angeles'
+            }
+          }
+        ]
+      }
+    }
+
+    channel_list_hash_cont = {
+        'body' => {
+            'channels' => [
+                {
+                    :channel_id => '125',
+                    :device_type => 'ios',
+                    :installed => true,
+                    :opt_in => false,
+                    :push_address => 'FE66489F304DC75B8D6E8200DFF8A456E8DAEACEC428B427E9518741C92C6660',
+                    :created => '2013-08-08T20:41:06',
+                    :last_registration => '2014-05-01T18:00:27',
+                    :alias => 'your_user_id',
+                    :tags => %w(tag1 tag2),
+                    :tag_groups => {
+                        :tag_group_1 => %w(tag1 tag2),
+                        :tag_group_2 => %w(tag1 tag2)
+                    },
+                    :ios => {
+                        :badge => 0,
+                        :quiettime => {
+                            :start => nil,
+                            :end => nil
+                        },
+                        :tz => 'America/Los_Angeles'
+                    }
+                },
+                {
+                    :channel_id => '126',
+                    :device_type => 'ios',
+                    :installed => true,
+                    :opt_in => false,
+                    :push_address => 'FE66489F304DC75B8D6E8200DFF8A456E8DAEACEC428B427E9518741C92C6660',
+                    :created => '2013-08-08T20:41:06',
+                    :last_registration => '2014-05-01T18:00:27',
+                    :alias => 'your_user_id',
+                    :tags => %w(tag1 tag2),
+                    :tag_groups => {
+                        :tag_group_1 => %w(tag1 tag2),
+                        :tag_group_2 => %w(tag1 tag2)
+                    },
+                    :ios => {
+                        :badge => 0,
+                        :quiettime => {
+                            :start => nil,
+                            :end => nil
+                        },
+                        :tz => 'America/Los_Angeles'
+                    }
+                }
+            ]
+        }
+    }
+
+    channel_id_list = %w(126 125 124 123)
+
+    it 'can iterate through a response' do
+      allow(airship)
+          .to receive(:send_request)
+                  .and_return(channel_list_hash, channel_list_hash_cont)
+
+      channel_list = UA::ChannelList.new(airship)
+      channel_list.each do |channel|
+        expect(channel[:channel_id]).to eq channel_id_list.pop
+      end
+    end
   end
 end
