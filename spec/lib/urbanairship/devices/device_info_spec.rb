1b306a2240c8113d05d30cb2b3291c04a1f5cf5d
kfrye <kris.frye@gmail.com>
2015-08-24 17:06:16 -0700

    Created tests for devicetoken, devicetokenlist, apid, and apidlist

--- spec/lib/urbanairship/devices/device_info_spec.rb
+++ spec/lib/urbanairship/devices/device_info_spec.rb
@@ -219,4 +219,152 @@ describe Urbanairship::Devices do
       end
     end
   end
+
+  describe Urbanairship::Devices::DeviceToken do
+    device_token = UA::DeviceToken.new(client: airship)
+
+    describe '#lookup' do
+      it 'fails when given a non-string token' do
+        expect {
+          device_token.lookup(token: 123)
+        }.to raise_error(ArgumentError)
+      end
+
+      it 'returns info on a device token correctly' do
+        expected_resp = {
+          'body' => {
+            'device_token' => 'device token',
+            'active' => true,
+            'alias' => 'alias',
+            'tags' => ['tag1', 'tag2'],
+            'created' => '2015-08-01',
+            'last_registration' => '2015-08-01',
+            'badge' => 2,
+            'quiettime' => {
+              'start' => '22:00',
+              'end' => '8:00'
+            },
+            'tz' => "America/Los_Angeles"
+          } ,
+          'code' => 200
+        }
+        allow(airship).to receive(:send_request).and_return(expected_resp)
+        actual_resp = device_token.lookup(token: 'token')
+        expect(actual_resp).to eq(expected_resp)
+      end
+    end
+  end
+
+  describe Urbanairship::Devices::DeviceTokenList do
+    token = {
+      'device_token' => 'device token',
+      'active' => true,
+      'alias' => 'alias',
+      'tags' => ['tag1', 'tag2'],
+      'created' => '2015-08-01',
+      'last_registration' => '2015-08-01',
+      'badge' => 2,
+      'quiettime' => {
+        'start' => '22:00',
+        'end' => '8:00'
+      },
+      'tz' => "America/Los_Angeles"
+    }
+    expected_resp = {
+      'body' => {
+        'device_tokens' => [token, token, token],
+        'device_tokens_count' => 3,
+        'next_page' => 'url'
+      },
+      'code' => 200
+    }
+    expected_next_response = {
+      'body' => {
+        'device_tokens' => [token, token, token],
+        'device_tokens_count' => 3
+      },
+      'code' => 200
+    }
+    it 'can iterate through a list correctly' do
+      allow(airship).to receive(:send_request).and_return(expected_resp, expected_next_response)
+      device_token_list = UA::DeviceTokenList.new(client: airship)
+      instantiated_list = Array.new
+      device_token_list.each do |t|
+        instantiated_list.push(t)
+        expect(token).to eq(t)
+      end
+      expect(instantiated_list.size).to eq(6)
+    end
+
+    it 'can return the list count' do
+      allow(airship).to receive(:send_request).and_return(expected_resp, 23)
+      device_token_list = UA::DeviceTokenList.new(client: airship)
+      count = device_token_list.count
+      expect(count).to eq(23)
+    end
+  end
+
+  describe Urbanairship::Devices::APID do
+    expected_resp = {
+      'body' => {
+        'apid' => 'apid',
+        'active' => true,
+        'alias' => 'alias',
+        'tags' => ['tag1', 'tag2'],
+        'created' => '2015-08-01',
+        'last_registration' => '2015-08-01',
+        'gcm_registration_id' => 'id'
+      },
+      'code' => 200
+    }
+    apid = UA::APID.new(client: airship)
+
+    it 'fails if apid is not a string' do
+      expect {
+        apid.lookup(123)
+      }.to raise_error(ArgumentError)
+    end
+
+    it 'returns apid info correctly' do
+      allow(airship).to receive(:send_request).and_return(expected_resp)
+      actual_response = apid.lookup(apid: 'apid')
+      expect(actual_response).to eq(expected_resp)
+    end
+  end
+
+  describe Urbanairship::Devices::APIDList do
+    apid = {
+      'apid' => 'apid',
+      'active' => true,
+      'alias' => 'alias',
+      'tags' => ['tag1', 'tag2'],
+      'created' => '2015-08-01',
+      'last_registration' => '2015-08-01',
+      'gcm_registration_id' => 'id'
+    }
+    expected_resp = {
+      'body' => {
+        'apids' => [apid, apid, apid],
+        'next_page' => 'url'
+      },
+      'code' => 200
+    }
+    expected_next_resp = {
+      'body' => {
+        'apids' => [apid, apid, apid],
+      },
+      'code' => 200
+    }
+
+    it 'correctly iterates over the list' do
+      allow(airship).to receive(:send_request).and_return(expected_resp, expected_next_resp)
+      apid_list = Urbanairship::APIDList.new(client: airship)
+      instantiated_list = Array.new
+      apid_list.each do |a|
+        instantiated_list.push(a)
+        expect(a).to eq(apid)
+      end
+      expect(instantiated_list.size).to eq(6)
+    end
+  end
 end
