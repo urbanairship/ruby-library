84e8eb40e0b2be48cdca7fe9941cb76506a598bb
Max Del Giudice <maxdelgiudice@gmail.com>
2016-04-26 11:21:53 -0700

    initial commit

--- lib/urbanairship/loggable.rb
+++ lib/urbanairship/loggable.rb
@@ -5,15 +5,18 @@ module Urbanairship
   module Loggable
 
     def logger
-      @logger ||= create_logger
+      Loggable.logger
     end
 
-    def create_logger
+    def self.logger
+      @logger ||= Loggable.create_logger
+    end
+
+    def self.create_logger
       logger = Logger.new('urbanairship.log')
       logger.datetime_format = '%Y-%m-%d %H:%M:%S'
       logger.progname = 'Urbanairship'
       logger
     end
-
   end
 end
