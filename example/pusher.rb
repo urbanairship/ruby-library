ef0938576c14d9a0e375a2b3649638b9915051b2
jenniferjsmmiller <jenniferjsmmiller@gmail.com>
2017-07-17 16:33:44 -0700

    Fixed test class

--- example/pusher.rb
+++ example/pusher.rb
@@ -4,13 +4,13 @@ class Pusher
 	UA = Urbanairship
 
 	def send_message
-		airship = UA::Client.new(key:'CxEeUYM6TqCEAY9FaVHTVw', secret:'g9JW4M4uRLmWUoaM-mZE6g')
+		airship = UA::Client.new(key:'app_key', secret:'master_secret')
 		p = airship.create_push
 		p.audience = UA.all
 		p.notification = UA.notification(alert: 'Hello')
 		p.device_types = UA.all
 		p.in_app = UA.in_app(
-				alert: 'it is I, Jenn',
+				alert: 'This is a test message!',
 				display_type: 'banner'
 				)
 		p.send_push
