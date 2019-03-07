d60702e14838b6a24e2ca9d54121377f974a0c03
Sarah Kirk <sarah.kirk@UA-C02XV5WQJHD3.skynet.urbanairship.com>
2019-03-07 14:32:54 -0800

    adds to the sms class

--- lib/urbanairship/devices/sms.rb
+++ lib/urbanairship/devices/sms.rb
@@ -5,11 +5,14 @@ module Urbanairship
     class Sms
       include Urbanairship::Common
       include Urbanairship::Loggable
+      attr_reader :msisdn, :sender, :opted_in
       attr_accessor :sender
 
       def initialize(client: required('client'))
         @client = client
         @sender = sender
+        @msisdn = nil
+        @opted_in = nil
       end
     end
   end
