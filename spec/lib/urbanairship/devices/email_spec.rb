9e4b80dccbbaf716f22746ab5e051b6b82be9181
Sarah Kirk <sarah.kirk@urbanairship.com>
2019-03-19 14:00:56 -0700

    adds scaffolding for first email test

---
+++ spec/lib/urbanairship/devices/email_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+require 'urbanairship'
+require 'urbanairship/devices/email'
+
+describe Urbanairship::Devices do
+  UA = Urbanairship
+  airship = UA::Client.new(key: '123', secret: 'abc')
+
+  describe Urbanairship::Devices::Email do
+    register_response = {
+      "ok": true,
+      "channel_id": "251d3318-b3cb-4e9f-876a-ea3bfa6e47bd"
+    }
+
+    describe '#register' do
+      it 'can register email channel' do
+        email_channel = UA::Email.new(client: airship)
+        email_channel.type = 'email'
+        email_channel.commercial_opted_in = '2018-10-28T10:34:22'
+        email_channel.address = 'finnthehuman@adventure.com'
+        email_channel.timezone = 'America/Los_Angeles'
+        email_channel.locale_country = 'US'
+        email_channel.locale_language = 'en'
+
+        allow(airship).to receive(:send_request).and_return(register_response)
+        actual_resp = email_channel.register
+        expect(actual_resp).to eq(register_response)
+      end
+    end
+  end
+end
