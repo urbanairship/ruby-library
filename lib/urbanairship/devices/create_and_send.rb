c423e64373703a80d5f2fad569064debe96661d8
Sarah Kirk <sarah.kirk@urbanairship.com>
2019-11-01 13:25:05 -0700

    -fleshes out test spec -begin formulating create and send email method -add to attr_accessors -add to initialize method

--- lib/urbanairship/devices/create_and_send.rb
+++ lib/urbanairship/devices/create_and_send.rb
@@ -5,19 +5,42 @@ module Urbanairship
     class CreateAndSend
       include Urbanairship::Common
       include Urbanairship::Loggable
-      attr_accessor :
+      attr_accessor :audience,
+                    :create_and_send,
+                    :ua_address,
+                    :ua_commercial_opeted_in,
+                    :ua_transactional_opted_in,
+                    :device_types,
+                    :notification,
+                    :campaigns,
+                    :email
 
       def initialize(client: required('client'))
         @client = client
+        @audience = nil
+        @create_and_send = nil
         @ua_address = nil
+        @ua_commercial_opeted_in = nil
+        @ua_transactional_opted_in = nil
+        @device_types = nil
+        @notification = nil
+        @campaigns = nil
+        @email = nil
       end
 
-      def create_and_send
+      def create_and_send()
 
       end
 
       def email_channel
-        fail ArgumentError, 'address must be set to register email channel' if @ua_address.nil?
+        fail ArgumentError, 'address must be set to register email channel' if @audience.nil?
+        fail ArgumentError, 'address must be set to register email channel' if @create_and_send.nil?
+        fail ArgumentError, 'address must be set to register email channel' if @device_types.nil?
+        fail ArgumentError, 'address must be set to register email channel' if @notification.nil?
+
+        payload = {
+          
+        }
       end
 
       def sms_channel
--- spec/lib/urbanairship/devices/create_and_send_spec.rb
+++ spec/lib/urbanairship/devices/create_and_send_spec.rb
@@ -1,3 +1,23 @@
 require 'spec_helper'
 require 'urbanairship'
 require 'urbanairship/devices/create_and_send'
+require 'urbanairship/devices/email'
+require 'urbanairship/devices/sms'
+
+describe Urbanairship::Devices do
+  UA = Urbanairship
+  airship = UA::Client.new(key: '123', secret: 'abc')
+
+  describe Urbanairship::Devices::CreateAndSend do
+
+    email_response = {
+        "ok": true,
+        "operation_id": "67c65146-c27f-431f-b54a-83aca694fdd3",
+        "push_ids": [
+            "c0eead17-333b-4f86-8a42-9fb7be1ed627"
+        ],
+        "message_ids": [],
+        "content_urls": []
+    }
+  end
+end
