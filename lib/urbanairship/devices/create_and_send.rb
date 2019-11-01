2cdac82e5a39a6a04097e2c8792017bfa802536b
Sarah Kirk <sarah.kirk@urbanairship.com>
2019-11-01 12:54:16 -0700

    begin scaffolding methods for cns

--- lib/urbanairship/devices/create_and_send.rb
+++ lib/urbanairship/devices/create_and_send.rb
@@ -8,10 +8,27 @@ module Urbanairship
       attr_accessor :
 
       def initialize(client: required('client'))
-
+        @client = client
+        @ua_address = nil
       end
 
       def create_and_send
+
+      end
+
+      def email_channel
+        fail ArgumentError, 'address must be set to register email channel' if @ua_address.nil?
+      end
+
+      def sms_channel
+
+      end
+
+      def mms_notification
+
+      end
+
+      def open_channels
 
       end
 
@@ -20,5 +37,5 @@ module Urbanairship
       end
 
       def operation
-        
+
       end
