009e1c4d030343dc523469af80337084bcf0b6fd
Sarah Kirk <sarah.kirk@urbanairship.com>
2019-11-04 14:07:49 -0800

    start params for email override

--- lib/urbanairship/devices/notification.rb
+++ lib/urbanairship/devices/notification.rb
@@ -5,17 +5,33 @@ module Urbanairship
     class CreateAndSend
       include Urbanairship::Common
       include Urbanairship::Loggable
-      attr_accessor :
+      attr_accessor :bcc,
+                    :bypass_opt_in_level,
+                    :html_body,
+                    :message_type,
+                    :plaintext_body,
+                    :reply_to,
+                    :sender_address,
+                    :sender_name,
+                    :subject
 
       def initialize(client: required('client'))
         @client = client
+        @bcc = nil
+        @bypass_opt_in_level = nil
+        @html_body = nil
+        @message_type = nil
+        @plaintext_body = nil
+        @reply_to = nil
+        @sender_address = nil
+        @sender_name = nil
+        @subject = nil
       end
 
       def email_override
       end
 
       def email_with_inline_template
-
       end
 
       def mms_platform_override
