f1864a34c462edce223390cb050a71a88c168db7
Gabriel Maffia <gabriel@monaeo.com>
2019-12-06 12:27:08 -0300

    Updates BASE_URL

--- lib/urbanairship/common.rb
+++ lib/urbanairship/common.rb
@@ -4,8 +4,8 @@ require 'urbanairship/loggable'
 module Urbanairship
   # Features mixed in to all classes
   module Common
-    SERVER = 'go.airship.com'
-    BASE_URL = 'https://go.airship.com/api'
+    SERVER = 'go.urbanairship.com'
+    BASE_URL = 'https://go.urbanairship.com/api'
     CHANNEL_URL = BASE_URL + '/channels/'
     OPEN_CHANNEL_URL = BASE_URL + '/channels/open/'
     DEVICE_TOKEN_URL = BASE_URL + '/device_tokens/'
