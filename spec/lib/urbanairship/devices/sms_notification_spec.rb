2c8a3a72d42684aca11c63b92d3f96caf09b089a
Sarah Kirk <sarah.kirk@urbanairship.com>
2020-01-16 13:37:49 -0800

    write two more tests for sms notification

--- spec/lib/urbanairship/devices/sms_notification_spec.rb
+++ spec/lib/urbanairship/devices/sms_notification_spec.rb
@@ -15,6 +15,24 @@ describe Urbanairship::Devices do
        }
   }
 
+  sms_template_with_fields = {
+    "sms" : {
+      "template" : {
+        "fields" : {
+          "alert" : "Hi, {{customer.first_name}}, your {{#each cart}}{{this.name}}{{/each}} are ready to pickup at our {{customer.location}} location!"
+        }
+      }
+    }
+  }
+
+  sms_template_with_id = {
+    "sms" : {
+      "template" : {
+        "template_id": "9335bb2a-2a45-456c-8b53-42af7898236a"
+      }
+    }
+  }
+
   describe Urbanairship::Devices::SmsNotification do
 
     describe '#sms_notification_override' do
@@ -26,6 +44,22 @@ describe Urbanairship::Devices do
         notification.shorten_links = true
         result = notification.sms_notification_override
         expect(result).to eq(sms_override)
+      end
+    end
+
+    describe '#sms_inline_template' do
+      it 'can format sms inline templates correctly' do
+        notification = UA::SmsNotification.new(client: airship)
+        notification.sms_alert = "Hi, {{customer.first_name}}, your {{#each cart}}{{this.name}}{{/each}} are ready to pickup at our {{customer.location}} location!"
+        result = notification.sms_inline_template
+        expect(result).to eq(sms_template_with_fields)
+      end
+
+      it 'can format sms inline templates id correctly' do
+        notification = UA::SmsNotification.new(client: airship)
+        notification.template_id = "9335bb2a-2a45-456c-8b53-42af7898236a"
+        result = notification.sms_inline_template
+        expect(result).to eq(sms_template_with_id)
       end
     end
   end
