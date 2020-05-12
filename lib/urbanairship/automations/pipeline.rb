030eef512fd7d0984faef35a4ca98b8dfe3ab51a
Sarah Kirk <sarah.kirk@urbanairship.com>
2020-05-12 16:13:04 -0700

    renames folder, begins scaffolding pipeline.rb

---
+++ lib/urbanairship/automations/pipeline.rb
@@ -0,0 +1,15 @@
+require 'urbanairship'
+
+module Urbanairship
+  module Automations
+    class Pipeline
+      include Urbanairship::Common
+      include Urbanairship::Loggable
+
+      def initialize
+      end
+
+      
+    end
+  end
+end
