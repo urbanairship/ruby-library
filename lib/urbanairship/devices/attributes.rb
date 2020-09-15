9c97545e8689ef160147ac8072322e6b43d44d9f
Sarah Kirk <sarah.kirk@urbanairship.com>
2020-09-15 14:06:50 -0700

    adds attr_accessors for attribute class

--- lib/urbanairship/devices/attributes.rb
+++ lib/urbanairship/devices/attributes.rb
@@ -5,12 +5,26 @@ module Urbanairship
         class Attributes
             include Urbanairship::Common
             include Urbanairship::Loggable
-            attr_accessor 
+            attr_accessor :attribute,
+                          :operator,
+                          :is_empty,
+                          :before,
+                          :after,
+                          :range,
+                          :equals,
+                          :precision,
+                          :value
 
             def initialize(client: required('client'))
                 @client = client
             end
-            
+
+            def payload
+                {
+                    
+                }
+            end
+
         end
     end
 end 
