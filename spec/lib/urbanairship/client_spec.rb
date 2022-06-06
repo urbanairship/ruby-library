f7aa0959fcba40b73464184275b2ea7e4a4bfb67
pdxmele <saxbarm@gmail.com>
2022-06-06 09:23:45 -0700

    fix merge mistake

--- spec/lib/urbanairship/client_spec.rb
+++ spec/lib/urbanairship/client_spec.rb
@@ -82,6 +82,6 @@ describe Urbanairship::Client do
                                .and_return(mock_response)
 
     ua_client = UA::Client.new(key: '123', token: 'test-token')
-    ua_client.send_request(method: 'POST', url: UA::Common::CHANNEL_URL, auth_type: :bearer)
+    ua_client.send_request(method: 'POST', url: UA.channel_path, auth_type: :bearer)
   end
 end
