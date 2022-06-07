6c086f808d94dc8080b3fde39fb3ac658903d118
pdxmele <saxbarm@gmail.com>
2022-06-07 14:38:12 -0700

    tests for bearer auth switch

--- spec/lib/urbanairship/client_spec.rb
+++ spec/lib/urbanairship/client_spec.rb
@@ -60,7 +60,7 @@ describe Urbanairship::Client do
       .to raise_error(ArgumentError, "path and url can't be both nil")
   end
 
-  it 'uses basic auth' do
+  it 'uses basic auth (by default when token not provided)' do
     mock_response = double('response')
     allow(mock_response).to(receive_messages(code: 200, headers: '', body: '{}'))
     expect(RestClient::Request).to(receive(:execute).with(hash_including({ user: '123', password: 'abc' })))
@@ -68,6 +68,11 @@ describe Urbanairship::Client do
 
     ua_client = UA::Client.new(key: '123', secret: 'abc')
     ua_client.send_request(method: 'POST', path: UA.channel_path)
+  end
+
+  it 'is instantiated with a "key" and "token"' do
+    ua_client = UA::Client.new(key: '123', token: 'test-token')
+    expect(ua_client).not_to be_nil
   end
 
   it 'uses bearer auth' do
@@ -83,5 +88,20 @@ describe Urbanairship::Client do
 
     ua_client = UA::Client.new(key: '123', token: 'test-token')
     ua_client.send_request(method: 'POST', path: UA.custom_events_path, auth_type: :bearer)
+  end
+
+  it 'automatically uses bearer auth when token provided' do
+    mock_response = double('response')
+    allow(mock_response).to(receive_messages(code: 200, headers: '', body: '{}'))
+
+    expected_headers = {
+      'X-UA-Appkey' => '123',
+      'Authorization' => 'Bearer test-token'
+    }
+    expect(RestClient::Request).to(receive(:execute).with(include(headers: include(expected_headers))))
+                               .and_return(mock_response)
+
+    ua_client = UA::Client.new(key: '123', token: 'test-token')
+    ua_client.send_request(method: 'POST', path: UA.custom_events_path)
   end
 end
